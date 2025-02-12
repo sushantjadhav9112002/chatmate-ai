7e51567d4b742fb345c96f0b0152da78da55465f
Sushant Jadhav <sushantjadhav9112002@gmail.com>
2025-02-12 13:56:09 +0530

    client url setup

--- Client/src/routes/signUpPage/signUppage.jsx
+++ Client/src/routes/signUpPage/signUppage.jsx
@@ -13,7 +13,7 @@ const Signuppage = () => {
     setError('');
 
     try {
-      const response = await fetch('http://localhost:3000/api/auth/register', {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/api/auth/register`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
